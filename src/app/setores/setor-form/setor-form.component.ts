c63ac29e5f446bcb212bb77bc6f81c3a6ef080b7
hugohasth <hugohasth@gmail.com>
2023-05-10 21:18:00 -0300

    Adicionado location para voltar ao clicar no botão cancelar e ao salvar para voltar à listagem

--- src/app/setores/setor-form/setor-form.component.ts
+++ src/app/setores/setor-form/setor-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { SetoresService } from '../services/setores.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-setor-form',
@@ -15,7 +16,8 @@ export class SetorFormComponent {
 	constructor(
 		private formBuilder: FormBuilder,
 		private service: SetoresService,
-		private snackBar: MatSnackBar) {
+		private snackBar: MatSnackBar,
+		private location: Location) {
 		this.form = this.formBuilder.group({
 			nome: [null],
 			porcentagem: [null],
@@ -28,11 +30,16 @@ export class SetorFormComponent {
 	}
 	
 	onSubmit() {
-		this.service.save(this.form.value).subscribe(result => console.log(result), error => this.onError());
+		this.service.save(this.form.value).subscribe(result => this.onSucces(), error => this.onError());
 	}
 	
 	onCancel() {
-		
+		this.location.back();
+	}
+	
+	onSucces() {
+		this.snackBar.open('Setor salvo com sucesso!', '', { duration: 5000 });
+		this.onCancel();
 	}
 	
 	onError() {
