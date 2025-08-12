4c621d14de88efbccb689564fa134bcf37696edb
Hugo Amaral Silva <hugohasth@gmail.com>
2025-08-12 19:07:24 -0300

    test: add validation tests and fix specs

--- src/app/setores/guard/setor.resolver.spec.ts
+++ src/app/setores/guard/setor.resolver.spec.ts
@@ -1,14 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { ResolveFn } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { setorResolver } from './setor.resolver';
+import { Setor } from '../model/setor';
 
 describe('setorResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+  const executeResolver: ResolveFn<Setor> = (...resolverParameters) =>
       TestBed.runInInjectionContext(() => setorResolver(...resolverParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
   });
 
   it('should be created', () => {
